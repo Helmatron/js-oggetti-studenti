daaceab3d338bbc9d196e2279d1197e43830c34f
Helmatron <ema.matro@gmail.com>
2024-03-24 14:35:09 +0100

    add: milestone 2 - personalizzata con form

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 // console.log('js-oggetti-studenti')
 
+// << MILESTONE 1 >>
+
 // Creare un array di oggetti di studenti.
 // Ogni oggetto studente deve avere tre proprietà: nome, cognome, eta
 
@@ -27,3 +29,19 @@ const arrayStudenti = [
 // prima ciclate con un ciclo for
 // poi commentate il ciclo for e usate il forEach
 arrayStudenti.forEach(studenti => console.log(studenti.nome, studenti.cognome));
+
+
+// << MILESTONE 2 >> 
+// (INVECE DI USARE IL PROMPT: mi volevo esercitare nel chiedere le info all'utente tramite un form e pusharle nell'array)
+
+
+// Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+
+//CHIEDO all'utente gli elementi e li prendo dal form
+const nomeUtente = getElement
+
+
+// Aggiungere il nuovo studente all’array di studenti
+// Ciclare su tutti gli studenti e stampare per ognuno di essi tutte le proprietà nome, cognome, eta
+// prima ciclate con un ciclo for
+// poi commentate il ciclo for e usate il forEach
