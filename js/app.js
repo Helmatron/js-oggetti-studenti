1b6a1d91fca0eae4b40900fd4bf160ea1dfa9d4a
Helmatron <ema.matro@gmail.com>
2024-03-24 13:43:09 +0100

    add: arrayStudenti

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-console.log('js-oggetti-studenti')
+// console.log('js-oggetti-studenti')
+
+// Creare un array di oggetti di studenti.
+// Ogni oggetto studente deve avere tre proprietà: nome, cognome, eta
+
+const arrayStudenti = [
+    {
+    nome: 'mario',
+    cognome: 'rossi',
+    eta: '18'
+    },
+    {
+        nome: 'giuseppe',
+        cognome: 'faina',
+        eta: '19'
+    },
+]
+// console.log(arrayStudenti)
+
+// Ciclare su tutti gli studenti e stampare, per ognuno di essi, nome e cognome.
+
+
+
+// prima ciclate con un ciclo for
+// poi commentate il ciclo for e usate il forEach
