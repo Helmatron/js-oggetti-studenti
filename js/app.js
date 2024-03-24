dfae99dc7e0b5e4ecad766fbd89dc2c74cad4ccf
Helmatron <ema.matro@gmail.com>
2024-03-24 13:44:07 +0100

    add: ciclo ForEach

--- js/app.js
+++ js/app.js
@@ -18,11 +18,12 @@ const arrayStudenti = [
 // console.log(arrayStudenti)
 
 // Ciclare su tutti gli studenti e stampare, per ognuno di essi, nome e cognome.
-for (let i = 0; i < arrayStudenti.length; i++) {
-    const studenti = arrayStudenti[i];
-     console.log(studenti.nome, studenti.cognome)
-}
+// for (let i = 0; i < arrayStudenti.length; i++) {
+//     const studenti = arrayStudenti[i];
+//      console.log(studenti.nome, studenti.cognome)
+// }
 
 
 // prima ciclate con un ciclo for
 // poi commentate il ciclo for e usate il forEach
+arrayStudenti.forEach(studenti => console.log(studenti.nome, studenti.cognome));
